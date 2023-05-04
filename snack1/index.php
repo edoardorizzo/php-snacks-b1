ea6e2699827113fbd0418f92b066acea38b41994
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-04 12:30:49 +0200

    remove vardump

--- snack1/index.php
+++ snack1/index.php
@@ -1,7 +1,7 @@
 <?php
 /* Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. */
 
-$matchs = [
+$matches = [
     [
         'squadra_casa' => 'Verona',
         'squadra_ospite' => 'Inter',
@@ -22,9 +22,8 @@ $matchs = [
     ]
     ];
 
-var_dump($matchs);
 
-foreach ($matchs as $match) {
+foreach ($matches as $match) {
     echo $match['squadra_casa'] . ' - ' . $match['squadra_ospite'] . ' ( ' . $match['punti_squadra_casa'] . ' - ' . $match['punti_squadra_ospite'] . ' ) '. ' / ';
   }
 
