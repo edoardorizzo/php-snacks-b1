e31a5d917126f41682e0be5ad132adcf9f0f9951
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-04 12:51:37 +0200

    snack done

---
+++ snack2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="GET" action="myscript.php">
+        <div>
+            <label for="name">Nome:</label>
+            <input type="text" id="name" name="name">
+        </div>
+        <div>
+            <label for="mail">Email:</label>
+            <input type="text" id="mail" name="mail">
+        </div>
+        <div>
+            <label for="age">Età:</label>
+            <input type="number" id="age" name="age">
+        </div>
+        <input type="submit" value="Invia">
+    </form>
+</body>
+
+</html>
